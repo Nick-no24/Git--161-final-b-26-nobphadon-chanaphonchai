17f3d40e1992e6c176ea7e94e268a6500ec690e6
Nick-no24 <nobphadon.chan@bumail.net>
2024-12-03 16:28:37 +0700

    Exam1

--- Assets/Exam1/Bird.cs
+++ Assets/Exam1/Bird.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bird : MonoBehaviour
+public class Bird : Animal
 {
+    private float dailyFoodConsumption = 0.5f;
+    private float weight = 1f;
+    private float FoodConsumption;
+    
+    
+    public override float CalculateFoodRequirement()
+    {
+        Debug.Log($"{animalName} eats {dailyFoodConsumption}kg of food  daily with and weight  {weight}kg.");
+        FoodConsumption = dailyFoodConsumption * weight*7;
+        return FoodConsumption;
+    }
+    public override void MakeSound()
+    {
+        Debug.Log($"The Animal Makes a sound.");
+        Debug.Log($"{animalName} tweet tweet");
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Init("Birds");
+        MakeSound();
+        CalculateFoodRequirement();
+        DisplayName();
+        Debug.Log($"{animalName}'s weekly food requirement {FoodConsumption}kg. ");
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Exam1/Dog.cs
+++ Assets/Exam1/Dog.cs
@@ -4,28 +4,33 @@ using UnityEngine;
 
 public class Dog : Animal
 {
-    private float dailyFoodConsumption;
-    private int activityLevel;
-    private float FoodConsumption;
+    private float dailyFoodConsumption =  0.5f;
+    private int activityLevel = 3;
+    private float _foodConsumption;
+    
     public override float CalculateFoodRequirement()
     {
-        FoodConsumption = dailyFoodConsumption * activityLevel;
-        Debug.Log()
-        return FoodConsumption;
+        Debug.Log($"{animalName} eats {dailyFoodConsumption}kg of food  daily with activitiy level {activityLevel}");
+        _foodConsumption = dailyFoodConsumption * activityLevel*7;
+        return _foodConsumption;
     }
-    
+    public override void MakeSound()
+    {
+        Debug.Log($"The Animal Makes a sound.");
+        Debug.Log($"{animalName} barks woof woof");
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        Init("dog");
+        Init("Dog");
+        MakeSound();
         CalculateFoodRequirement();
         DisplayName();
+        Debug.Log($"{animalName}'s weekly food requirement {_foodConsumption}kg. ");
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
